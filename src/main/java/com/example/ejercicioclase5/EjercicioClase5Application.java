cd3f013f8d733d78f0d1dceae0483975c99fec33
alex-segovia <a20213849@pucp.edu.pe>
2024-04-26 20:24:41 -0500

    Primer commit

---
+++ src/main/java/com/example/ejercicioclase5/EjercicioClase5Application.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioclase5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioClase5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioClase5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ejercicioclase5/EjercicioClase5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioclase5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioClase5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
